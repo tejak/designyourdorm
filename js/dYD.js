7c3b386241563b263f00a8ead8fda838d342cc8b
Courtneyyadoo <courtney.yadoo@gmail.com>
2015-07-01 18:50:47 +0000

    Color menu

--- js/dYD.js
+++ js/dYD.js
@@ -1,3 +1,39 @@
 $(function(){
   $(".dragged").draggable();
+  
+  $(".wallcolor-picker").change(function(){
+    console.log($(this).val());
+    if($(this).val() === "Blue"){
+       $(".room-container").css("background-color","blue");
+       }
+    if($(this).val() === "Purple"){
+       $(".room-container").css("background-color","purple");
+       }
+    if($(this).val() === "Yellow"){
+       $(".room-container").css("background-color","yellow");
+       }
+    if($(this).val() === "Pink"){
+       $(".room-container").css("background-color","pink");
+       }
+  });
+  
+  $(".sidecolor-picker").change(function(){
+    console.log($(this).val());
+    if($(this).val() === "Blue"){
+       $(".room-container").css("border-left", "100px solid #7B88E8");
+       $(".room-container").css("border-right", "100px solid #7B88E8");
+       }
+    if($(this).val() === "Purple"){
+       $(".room-container").css("border-left", "100px solid purple");
+       $(".room-container").css("border-right", "100px solid purple");
+       }
+    if($(this).val() === "Yellow"){
+       $(".room-container").css("border-left", "100px solid yellow");
+       $(".room-container").css("border-right", "100px solid yellow");
+       }
+    if($(this).val() === "Pink"){
+       $(".room-container").css("border-left", "100px solid pink");
+       $(".room-container").css("border-right", "100px solid pink");
+       }
+  });
 });

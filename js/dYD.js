6c70550949331b59af90e80b60d9f399bf143ff2
adfav <Andrew.Favaloro@gmail.com>
2015-07-01 19:01:39 +0000

    ADFav work on homepage

--- js/dYD.js
+++ js/dYD.js
@@ -1,3 +1,20 @@
 $(function(){
   $(".dragged").draggable();
+  
+  /*$(".dragged").hover(function(){
+    $(this).css("cursor","pointer");}, function(){
+    $(this).css("cursor","auto");
+  });*/
+  
+  $(".dragged").mouseenter(function(){
+    $(this).css("cursor","pointer");
+  });
+  
+  $(".dragged").click(function(){
+    $(this).css("cursor","move");
+  });
+  
+  $(".dragged").mouseout(function(){
+    $(this).css("cursor","auto");
+  });
 });
